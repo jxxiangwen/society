c0d5e46615de65943c2c4d26e8be695236fdf2af
jxxiangwen@live.com <Github@4ever>
2016-04-13 16:06:07 +0800

    新增user表结构

--- src/main/java/cn/edu/shu/society/service/impl/UserServiceImpl.java
+++ src/main/java/cn/edu/shu/society/service/impl/UserServiceImpl.java
@@ -20,37 +20,30 @@ public class UserServiceImpl implements UserService {
     @Resource
     private UserDao userDao;
 
-    @Override
     public User findById(int id) {
         return userDao.findById(id);
     }
 
-    @Override
     public User login(User user) {
         return userDao.login(user);
     }
 
-    @Override
     public List<User> find(Map<String, Object> map) {
         return userDao.find(map);
     }
 
-    @Override
     public Long getTotal(Map<String, Object> map) {
         return userDao.getTotal(map);
     }
 
-    @Override
     public int update(User user) {
         return userDao.update(user);
     }
 
-    @Override
     public int add(User user) {
         return userDao.add(user);
     }
 
-    @Override
     public int delete(Integer id) {
         return userDao.delete(id);
     }
