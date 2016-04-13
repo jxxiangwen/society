17e1af2bd7510697a443d780529afae76868884d
jxxiangwen@live.com <Github@4ever>
2016-04-13 16:18:28 +0800

    新增user表结构

--- src/main/java/cn/edu/shu/society/util/PageBean.java
+++ src/main/java/cn/edu/shu/society/util/PageBean.java
@@ -9,7 +9,7 @@ public class PageBean {
     private int pageSize; // 每页记录数
     private int start;  // 起始页
 
-    public PageBean(int page, int pagSize) {
+    public PageBean(int page, int pageSize) {
         super();
         this.page = page;
         this.pageSize = pageSize;
